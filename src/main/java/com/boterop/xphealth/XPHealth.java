4e1926cd2ae2f1ef3fc3c71d26d42a00bfad25d2
Santiago Botero <98826652+boterop@users.noreply.github.com>
2024-04-29 13:34:53 -0500

    Create xp event (#9)
    * Add .vscode in .gitignore
    
    * Mod working

--- src/main/java/com/boterop/xphealth/XPHealth.java
+++ src/main/java/com/boterop/xphealth/XPHealth.java
@@ -2,8 +2,10 @@ package com.boterop.xphealth;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod("xphealth")
+@Mod(XPHealth.MOD_ID)
 public class XPHealth {
+    public static final String MOD_ID = "xphealth";
+
     public XPHealth() {
     }
 }
